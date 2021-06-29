0f08a3422214ab11df874cb94f9d4932cafdf590
김한철 <05.hanch.it@gmail.com>
2021-06-29 13:23:26 +0900

    through2-map 없이 진행

--- nodejs/http-uppercaserer.js
+++ nodejs/http-uppercaserer.js
@@ -1,8 +1,12 @@
 const http = require('http');
-const map = require('through2-map');
 
 const server = http.createServer((req, res) => {
-  req.pipe(map((data) => data.toString().toUpperCase())).pipe(res);
+  req.on('data', (d) => {
+    res.write(d.toString().toUpperCase());
+  });
+  req.on('end', () => {
+    res.end();
+  });
 });
 
 server.listen(process.argv[2]);
